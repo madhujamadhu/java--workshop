19644f6ebccacecdec8271aa3c58644966b5d8eb
madhu <madhujak24cse@vetias.ac.in>
2025-07-10 09:48:25 +0530

    basic

--- src/com/java/workshop/basic/bean/Mark.java
+++
@@ -1,9 +0,0 @@
-public class Mark {
-    public static void main(String[] args) {
-        int[] marks={50,78,69,98,89};
-        for(int mark:marks){
-            System.out.println("mark:"+mark);
-        }
-    }
-    
-}
