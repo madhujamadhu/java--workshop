c4e57cbcac6aea75b2dc43f43b8896cfb3c9fb05
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-10 10:12:19 +0530

    string

---
+++ src/main/com/java/workshop/basic/bean/strings/Stringss.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class Stringss {
+     public static void main(String[] args) {
+        String firstname="madhuja";
+        String lastname="madhu";
+        String finalname=firstname+" "+lastname;
+        System.out.println(finalname);
+        System.out.println(finalname.toUpperCase());
+    }
+    
+    
+}
