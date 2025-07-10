26384af536cd6aa258c2197cd4d849e0527d9136
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-10 14:42:41 +0530

    list types

--- src/main/java/com/java/workshop/collections/ListExample.java
+++ src/main/java/com/java/workshop/collections/ListExample.java
@@ -1,6 +1,7 @@
 package com.java.workshop.collections;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 public class ListExample {
     public static void main(String[] args) {
@@ -11,8 +12,13 @@ public class ListExample {
         names.add("ajee");
         names.add("vinu");
         System.out.println(names);
+        names.set(1, "shaha");
         for(int i=0;i<names.size();i++){
             System.out.println("name:"+names.get(i));
         }
+        names.sort(Comparator.naturalOrder());
+        System.out.println("sorted names:"+names);
+        names.sort(Comparator.reverseOrder());
+        System.out.println("Reverse sorted names:"+names);
     }
 }
