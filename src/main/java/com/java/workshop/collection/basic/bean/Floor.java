42fc206145557e1885f1bdd17222b48b423334e5
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-11 10:13:54 +0530

    tem

---
+++ src/main/java/com/java/workshop/collection/basic/bean/Floor.java
@@ -0,0 +1,20 @@
+package com.java.workshop.collection.basic.bean;
+
+public class Floor {
+    private String name;
+    private int number;
+    public void Name(String aName){
+        name=aName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void Number(int aNumber){
+        number=aNumber;
+    }
+    public int getdNumber(){
+        return number;
+    }
+    
+    
+}
