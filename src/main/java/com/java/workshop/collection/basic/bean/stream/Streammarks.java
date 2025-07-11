3fd6859c085c9f096ddffe75ecf6200e6afed977
madhujamadhu <madhujak24cse@vetias.ac.in>
2025-07-11 15:01:52 +0530

    streamaccdes.

---
+++ src/main/java/com/java/workshop/collection/basic/bean/stream/Streammarks.java
@@ -0,0 +1,18 @@
+package com.java.workshop.collection.basic.bean.stream;
+import java.util.Arrays;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Streammarks {
+    public static void main(String[] args) {
+        List<Integer> marks=Arrays.asList(98,65,89,76,44);
+        List<Integer> sortedMarks=marks.stream().sorted().toList();
+        System.out.println("sorted marks"+sortedMarks);
+        List<Integer> reversdec=marks.stream().sorted(Comparator.reverseOrder()).toList();
+        System.out.println("Revers marks:"+reversdec);
+        System.out.println("Hight marks:"+reversdec.get(0));
+
+    }
+    
+}
